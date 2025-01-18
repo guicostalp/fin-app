c97b4cae890dbbd333167842726673028356cbd1
Gui Costa <gui.costalp@gmail.com>
2025-01-18 14:27:57 +1300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate, Navigate } from 'react-router-dom';
+import { Route, Routes, useNavigate, Navigate, Router } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import LandingPage from '../pages/LandingPage';
 import MainPage from '../pages/MainPage';
@@ -28,21 +28,23 @@ function App() {
   }, [isAuthenticated, navigate]);
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LandingPage onLogin={handleLogin} />} />
-        <Route
-          path="/main"
-          element={isAuthenticated ? <MainPage onLogout={handleLogout} /> : <Navigate to="/" />}
-        />
-        {/* Add additional routes here as needed */}
-        {/* Example of a protected route */}
-        <Route
-          path="/protected"
-          element={isAuthenticated ? <div>Protected Content</div> : <Navigate to="/" />}
-        />
-      </Routes>
-    </div>
+    <Router basename="/fin-app">
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage onLogin={handleLogin} />} />
+          <Route
+            path="/main"
+            element={isAuthenticated ? <MainPage onLogout={handleLogout} /> : <Navigate to="/" />}
+          />
+          {/* Add additional routes here as needed */}
+          {/* Example of a protected route */}
+          <Route
+            path="/protected"
+            element={isAuthenticated ? <div>Protected Content</div> : <Navigate to="/" />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
