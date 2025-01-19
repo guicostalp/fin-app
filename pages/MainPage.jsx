c54556a30bb604b9e3b01d2fc29020d5b307a43d
Gui Costa <gui.costalp@gmail.com>
2025-01-19 16:12:57 +1300

    first commit

--- pages/MainPage.jsx
+++ pages/MainPage.jsx
@@ -1,9 +1,26 @@
 import PropTypes from 'prop-types'; // Import PropTypes
+import { useEffect, useState } from 'react';
 
 function MainPage({ onLogout }) {
+  const lastVisit = useState(localStorage.getItem('lastVisit'));
+
+  useEffect(() => {
+
+    // Update the last visit time in localStorage
+     const now = new Date();
+     localStorage.setItem('lastVisit', now.toISOString());
+
+    // Clear localStorage
+    //localStorage.clear();
+  }, []);
+
+
+
+
   return (
     <div>
       <h1>Welcome to the Main Page!</h1>
+      <h2>Your last visit: {lastVisit}</h2>
       <button onClick={onLogout}>Logout</button>
     </div>
   );
